45d35f5d889cb141841d4c19cb723a1227a56552
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-02 18:23:41 +0200

    iteration#1

---
+++ iteration1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//ITERATION 1
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log('El primer número es más grande que el segundo')
+    } else {
+        console.log('El segundo número es más grande que el primero')
+    }
+  }
+
+  sum(2, 10)
