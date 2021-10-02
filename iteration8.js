8012189dcdc298a108febad8c1dc0123f471405e
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-02 20:52:04 +0200

    iteration8

---
+++ iteration8.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//ITERATION 8
+
+const counterWords = [
+    'code',
+    'repeat',
+    'eat',
+    'sleep',
+    'code',
+    'enjoy',
+    'sleep',
+    'code',
+    'enjoy',
+    'upgrade',
+    'code'
+  ];
+
+let counter = 0;
+
+function repeatCounter(array, word) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].includes(word)) {
+            counter += 1;
+        }
+    }
+    console.log(`El término que has introducido se repite ${counter} veces en el array introducido`)
+  }
+
+repeatCounter(counterWords, 'code')
