c4a9dedf3bb90fd15b3b7b9b031d5e980dded662
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-03 18:01:16 +0200

    iteration2completed

--- iteration2.js
+++ iteration2.js
@@ -4,17 +4,18 @@
 
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 let lengths  = [];
-
+let longest = []
 function findLongestWord() {
     for(let palabra of avengers) {
-  lengths.push(palabra.length);
+        lengths.push(palabra.length);
     }
 
 let maximo = Math.max.apply(null, lengths);
 
     for (let elemento of avengers) {
     if (elemento.length === maximo) {
-        console.log(elemento);
+        longest.push(elemento)
     }}
+    console.log(longest[0])
 }
 findLongestWord()
