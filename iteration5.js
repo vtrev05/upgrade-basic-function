3af2a651af4807b42ffe91cca6ce4a56ad703f72
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-03 18:07:25 +0200

    iteration5 completed

--- iteration5.js
+++ iteration5.js
@@ -6,16 +6,20 @@ const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 
 let sum = 0;
 let lengths = 0
+let typesOfNumber = []
+let typesOfString = []
 
 function average(a) {
     for (let i = 0; i < a.length; i++) {
         if (typeof a[i] === 'number') {
+            typesOfNumber.push(a[i])
             sum += a[i]
         } else{
+            typesOfString.push(a[i])
             lengths += a[i].length
         }
     }
-    console.log(sum)
-    console.log(lengths)
+    console.log(sum / typesOfNumber.length)
+    console.log(lengths / typesOfString.length)
 }
 average(mixedElements)
